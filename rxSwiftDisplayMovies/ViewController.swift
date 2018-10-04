3d003561c4c0cfb4b725b513f0f6dda03386c1a4
OSX <osx@osxs-macbook-pro.local>
2018-10-04 22:06:24 +0200

    Initial Commit

---
+++ rxSwiftDisplayMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rxSwiftDisplayMovies
+//
+//  Created by OSX on 10/4/18.
+//  Copyright © 2018 DDSCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
